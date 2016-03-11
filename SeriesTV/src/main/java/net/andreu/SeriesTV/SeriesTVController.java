ce0123a2e5f42e9c8026c4f0384ccfeb231ce880
Andreu <Andreu@Andreu1>
2016-03-11 10:29:11 +0100

    agafa temporades

--- SeriesTV/src/main/java/net/andreu/SeriesTV/SeriesTVController.java
+++ SeriesTV/src/main/java/net/andreu/SeriesTV/SeriesTVController.java
@@ -1,14 +1,20 @@
 package net.andreu.SeriesTV;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javafx.event.ActionEvent;
 
@@ -53,19 +59,38 @@ public class SeriesTVController {
         buscar.sendKeys(nom);
         buscar.submit();
         
-        WebElement seria = navegador.findElement(By.className("ict"));
-        seria.click();
-        
-		/*WebElement img = navegador.findElement(By.id("port_serie"));
-        //String imatge = img.getAttribute("src");
-		Image imatge = new Image(img.getAttribute("src"));
-        imgSeria.setImage(imatge);*/
-        
-		for(WebElement t : navegador.findElements(By.xpath("//h2/u"))){
-			llistaTemporades.getItems().add(t.getText());
+        WebElement boxSeries = navegador.findElement(By.cssSelector("div.date-outer div.post-header"));
+		WebElement divSeries = boxSeries.findElement(By.cssSelector("div:nth-child(3)"));
+		List<WebElement> series = divSeries.findElements(By.tagName("img"));
+
+		if (series.size() > 1 || series.size() == 0) {
+
+			navegador.close();
+			
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Aletra:");
+			alert.setHeaderText("Error, el nom de la seria que ha introduit no existeix o correspon a mes d'una sera.");
+			alert.showAndWait();
+
+		}else{
+
+			WebElement serie = divSeries.findElement(By.xpath("./div[1]/div/a"));
+			serie.click();
+
+			WebDriverWait wait = new WebDriverWait(navegador, 30);
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@id, 'post-body-')]")));
+
+			for (WebElement temporades : navegador.findElements(By.xpath("//h2/u"))) {
+				llistaTemporades.getItems().add(temporades.getText());
+			}
+			
+			/*WebElement img = navegador.findElement(By.id("port_serie"));
+	        //String imatge = img.getAttribute("src");
+			Image imatge = new Image(img.getAttribute("src"));
+	        imgSeria.setImage(imatge);*/
 		}
 	}
-	
+
 	// Event Listener on ComboBox[#llistaTemporades].onAction
 	@FXML
 	public void triarTemporada(ActionEvent event) {
